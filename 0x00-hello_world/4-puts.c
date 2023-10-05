c81db602b59ebee320a26f65f4d13ec89bdbc132
Osilamah1798 <emmanuelugboaga0@gmail.com>
2023-10-05 15:56:18 -0700

    solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
-{	char myString[]="\"Programming is like building a multilingual puzzle";
-	puts (myString);
+	int main(void)
+{	char myString[] = "\"Programming is like building a multilingual puzzle";
+	puts(myString);
 
-	return(0) ;
+	return (0);
 }
