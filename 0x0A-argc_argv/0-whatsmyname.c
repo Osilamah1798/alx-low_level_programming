284dcbfc0934ffdf1e625018aa5bc2ca9d48c293
Osilamah1798 <emmanuelugboaga0@gmail.com>
2024-01-12 05:42:24 -0800

    I am back baby args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Write a prog prints it's name
+ * Followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+        int main(int argc __attribute__((unused)), char *argv[])
+
+        {
+        printf("%s\n", *argv);
+        
+
+        return(0);
+        }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Write a prog that prints number of args passed into it 
+ * Followed by a new line
+ * 
+ * Return: Always 0 Success
+ */
+
+	int main(int argc, char *argv[] __attribute__((unused)))
+
+	{
+				
+		printf("%d\n", argc - 1);
+
+	return(0);
+	}
